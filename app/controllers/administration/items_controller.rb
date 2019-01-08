3988ec70e318a22d57ada2f235fae21335e6c67e
Florian <florian_pinto@outlook.fr>
2019-01-08 16:50:13 +0100

    Add controller update modification for change the percentage

--- app/controllers/administration/items_controller.rb
+++ app/controllers/administration/items_controller.rb
@@ -7,7 +7,21 @@ module Administration
     end
 
     def update
+      @item = Item.find(params[:id])
+      puts @item.id
+      item_params = params.require(:item).permit(:discount_percentage)
+      @item.update(item_params)
+
+      if @item.discount_percentage.positive?
+        @item.update(has_discount: true)
+      elsif @item.discount_percentage = 0
+        @item.update(has_discount: false)
+      end
       redirect_to administration_items_path
+      # respond_to do |format|
+      #  format.html { administration_item_path(params[:id]) }
+      #  format.js
+      # end
     end
   end
 end
