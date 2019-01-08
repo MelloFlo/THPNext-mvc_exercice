152b3edfa1a91b0ab787f5837d1fb702c5a2939a
Florian <florian_pinto@outlook.fr>
2019-01-08 17:58:08 +0100

    Add test for attribute of Category model

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: categories
+#
+#  id          :bigint(8)        not null, primary key
+#  name        :string           not null
+#  description :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+  describe 'Model instantiation' do
+    subject(:new_item) { described_class.new }
+
+    describe 'Database' do
+      it { is_expected.to have_db_column(:name).of_type(:string) }
+      it { is_expected.to have_db_column(:description).of_type(:text) }
+    end
+  end
+end
