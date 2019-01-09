cd40b0139ff28d310e3062234db71332c60ec55a
Florian <florian_pinto@outlook.fr>
2019-01-09 14:28:29 +0100

    Modification of alert message, and add condition percentage

--- app/controllers/administration/items_controller.rb
+++ app/controllers/administration/items_controller.rb
@@ -9,15 +9,18 @@ module Administration
     def update
       @item = Item.find(params[:id])
       puts @item.id
-      item_params = params.require(:item).permit(:discount_percentage)
-      @item.update(item_params)
-
+      if params[:item][:discount_percentage].to_f > 100
+        flash[:error] = "Veuillez entrer une réduction valable"
+      else
+        item_params = params.require(:item).permit(:discount_percentage)
+        @item.update(item_params)
+        flash[:notice] = "L'item a bien été modifié"
+      end
       if @item.discount_percentage.positive?
         @item.update(has_discount: true)
       elsif @item.discount_percentage = 0
         @item.update(has_discount: false)
       end
-      flash[:notice] = "L'item a bien été modifié"
       redirect_to administration_items_path
       # respond_to do |format|
       #  format.html { administration_item_path(params[:id]) }
