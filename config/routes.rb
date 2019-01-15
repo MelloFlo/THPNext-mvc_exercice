f26cf395deb7c1430ad3dfdcfb897fedb81ddb02
Florian <florian_pinto@outlook.fr>
2019-01-15 11:25:47 +0100

    Add Admin and User routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
+  devise_for :admins
   root 'administration/items#index'
 
   get '/home', to: 'home#landing_page'
