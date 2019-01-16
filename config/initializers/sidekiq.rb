4c2b5e76c152b487f3e087d12b35e98820da836a
Florian <florian_pinto@outlook.fr>
2019-01-16 15:49:45 +0100

    Add sidekiq credential

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: Rails.application.credentials.sidekiq[:REDIS_URL] }
+  config.redis = { url: Rails.application.credentials[:REDIS_URL] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: Rails.application.credentials.sidekiq[:REDIS_URL] }
+  config.redis = { url: Rails.application.credentials[:REDIS_URL] }
 end
