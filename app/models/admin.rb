d4e50ce5aaf85f781832d589418d64aeeaa9fd21
Florian <florian_pinto@outlook.fr>
2019-01-16 15:48:25 +0100

    Add sidekiq configuration

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -24,4 +24,8 @@ class Admin < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable
+
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
+  end
 end
